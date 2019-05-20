5e2a5e1b05a07ff2153524a18fd47fa6a84a9930
Guillaume Wallet <wallet.guillaume@gmail.com>
2019-05-20 21:01:23 +0200

    FIX move to CTest

--- test.sh
+++ test.sh
@@ -1,33 +1,11 @@
 #!/bin/sh
 
-ROOT_DIR=$(dirname $(readlink -f $0))
-BUILD_DIR=$ROOT_DIR/build
-TESTS_DIR=$ROOT_DIR/test.d
-TEST_SRC_DIR=$TESTS_DIR/src
-
-set -e
-$ROOT_DIR/build.sh
-set +e
-
-export PATH=$BUILD_DIR:$PATH
-
-cd $ROOT_DIR
-TESTS_FAILED=()
-for TEST in $(ls -p1 $TESTS_DIR | grep -v /); do
-  /bin/bash $TESTS_DIR/$TEST
-  TEST_RESULT=$?
-  if [ $TEST_RESULT != 0 ]; then
-    echo -e "[\e[31m✘\e[0m] $TEST test failed"
-    TESTS_FAILED+=($TEST)
-  else
-    echo -e "[\e[32m✔\e[0m] $TEST test passed"
-  fi
-done
-
-if [ ${#TESTS_FAILED[@]} -gt 0 ]; then
-  echo "There was ${#TESTS_FAILED[@]} test failures: ${TESTS_FAILED[@]}"
-  exit 1
-else
-  echo "All test passed!"
+if [ ! -d build ]; then
+	mkdir build
+	cd build
+	cmake ..
+	cd -
 fi
 
+cd build
+make test
